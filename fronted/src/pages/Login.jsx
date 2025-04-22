454a21376721011245a75445f8ba23f8d9101e28
dcodingdev <162737402+dcodingdev@users.noreply.github.com>
2025-04-23 01:20:18 +0530

    Update Login.jsx

--- fronted/src/pages/Login.jsx
+++ fronted/src/pages/Login.jsx
@@ -18,7 +18,7 @@ export default function Login() {
     setError("");
 
     try {
-      const res = await axios.post("http://localhost:3001/user/login", formData);
+      const res = await axios.post("https://booknest-backend-44av.onrender.com/user/login", formData);
       localStorage.setItem("token", res.data.token);
       navigate("/");
     } catch (err) {
