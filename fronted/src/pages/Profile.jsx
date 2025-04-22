5a50966f706db615cead8134f47d752a0edf4dda
dcodingdev <162737402+dcodingdev@users.noreply.github.com>
2025-04-23 01:21:05 +0530

    Update Profile.jsx

--- fronted/src/pages/Profile.jsx
+++ fronted/src/pages/Profile.jsx
@@ -1,66 +1,4 @@
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
-// import { useNavigate } from "react-router-dom";
-// import { Spinner } from "../components/Spinner";
-// import { PORT } from "../../../backend/config";
 
-// export default function Profile() {
-//   const [user, setUser] = useState(null);
-//   const [loading, setLoading] = useState(false);
-//   const navigate = useNavigate();
-
-//   useEffect(() => {
-//     const fetchUser = async () => {
-//       const token = localStorage.getItem("token");
-
-//       if (!token) {
-//         navigate("/user/login");
-//         return;
-//       }
-
-//       setLoading(true);
-//       try {
-//         const response = await axios.get(`http://localhost:${PORT}/user/profile`, {
-//           headers: { Authorization: `Bearer ${token}` },
-//         });
-
-//         setUser(response.data);
-//       } catch (err) {
-//         console.error("Error fetching user profile:", err);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchUser();
-//   }, [navigate]);
-
-//   useEffect(() => {
-//     if (!loading && !user) {
-//       navigate("/user/login");
-//     }
-//   }, [loading, user, navigate]);
-
-//   if (loading) {
-//     return <Spinner />;
-//   }
-
-//   return (
-//     <div className="p-4">
-//       {user ? (
-//         <div className="w-96 p-6 shadow-lg bg-white rounded-md text-center">
-//           <h2 className="text-2xl font-semibold mb-4">Profile</h2>
-//           <p><strong>Name:</strong> {user.name}</p>
-//           <p><strong>Email:</strong> {user.email}</p>
-//         </div>
-//       ) : (
-//         <p>No user information available.</p>
-//       )}
-//     </div>
-//   );
-// }
-
-// 
 
 import React, { useEffect, useState } from "react";
 import axios from "axios";
@@ -83,7 +21,7 @@ export default function Profile() {
       }
 
       try {
-        const response = await axios.get(`http://localhost:3001/user/profile`, {
+        const response = await axios.get(`https://booknest-backend-44av.onrender.com/user/profile`, {
           headers: { Authorization: `Bearer ${token}` },
         });
 
